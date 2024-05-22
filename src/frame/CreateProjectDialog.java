3133ba84fbd029d1a694f69fcd776c1db4e6c62c
dkTkrkdhfl <482gate@gmail.com>
2024-05-22 19:51:30 +0900

    폴더 풀어서 다시 올린 버전

---
+++ src/frame/CreateProjectDialog.java
@@ -0,0 +1,121 @@
+package frame;
+
+import style.ProjColor;
+import style.ProjStyleButton;
+import javax.swing.*;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.event.*;
+
+public class CreateProjectDialog extends JDialog { // Modal 창 만들기 위해
+	
+	private MainFrame parentFrame;
+	
+	public CreateProjectDialog(MainFrame parentFrame) {
+		this.parentFrame = parentFrame;
+		setTitle("New Project");
+		setSize(560, 480);
+		setLocationRelativeTo(null); // 화면 중앙 위치
+		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		setModal(true);
+		setUndecorated(true); // 타이틀바 제거
+		setOpacity(0.0f); // 처음엔 안보이게
+		
+		JPanel panel1 = new JPanel();
+		panel1.setBackground(ProjColor.customGray);
+		panel1.setLayout(null);
+		
+		JLabel lbl1 = new JLabel("New Project");
+		lbl1.setFont(new Font(null, Font.PLAIN, 50));
+		panel1.add(lbl1);
+		lbl1.setBounds(140, 1, 400, 80);
+		
+		JLabel lbl2 = new JLabel("project name");
+		lbl2.setFont(new Font(null, Font.PLAIN, 15));
+		panel1.add(lbl2);
+		lbl2.setBounds(35, 55, 400, 80);
+		
+		JLabel lbl3 = new JLabel("description");
+		lbl3.setFont(new Font(null, Font.PLAIN, 15));
+		panel1.add(lbl3);
+		lbl3.setBounds(35, 158, 400, 80);
+		
+		JTextField tf1 = new JTextField();
+		tf1.setBackground(ProjColor.customDarkGray);
+		tf1.setFont(new Font("맑은 고딕", Font.BOLD, 20));
+		tf1.setBorder(null);
+		panel1.add(tf1);
+		tf1.setBounds(37, 116, 465, 56);
+		
+		JTextArea ta1 = new JTextArea();
+		ta1.setBackground(ProjColor.customDarkGray);
+		ta1.setFont(new Font("맑은 고딕", Font.PLAIN, 15));
+		panel1.add(ta1);
+		ta1.setBounds(37, 216, 465, 150);
+		
+		ProjStyleButton btn1 = new ProjStyleButton(ProjColor.customDarkGray, ProjColor.clickedCustomDarkGray, Color.BLACK, "Create");
+		panel1.add(btn1);
+		btn1.setBounds(108, 380, 111, 56);
+		btn1.setPreferredSize(new Dimension(111, 56));
+		
+		btn1.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseReleased(MouseEvent e) {
+				String title = tf1.getText().trim();
+				String description = ta1.getText().trim();
+				
+				if(title.equals("")) {
+					JOptionPane.showMessageDialog(CreateProjectDialog.this, "Title cannot have empty spaces", "Error", JOptionPane.ERROR_MESSAGE);
+				}
+				else {
+					parentFrame.addProjectList(title, description);
+					parentFrame.repaintButtonPanel();
+					setVisible(false);
+					dispose();
+				}
+			}
+		});
+		
+		ProjStyleButton btn2 = new ProjStyleButton(ProjColor.customWhiteRed, ProjColor.clickedCustomWhiteRed, Color.BLACK, "Cancel");
+		panel1.add(btn2);
+		btn2.setBounds(325, 380, 111, 56);
+		btn2.setPreferredSize(new Dimension(111, 56));
+		
+		System.out.println("33");
+		
+		btn2.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseReleased(MouseEvent e) {
+				setVisible(false);
+				dispose();
+			}
+		});
+		
+		Timer timer = new Timer(1, new ActionListener() {
+            float opacity = 0.0f;
+
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if (opacity < 1.0f) {
+                    opacity += 0.1f;
+                    setOpacity(Math.min(opacity, 1.0f));
+                } else {
+                    ((Timer) e.getSource()).stop();
+                }
+            }
+        });
+        timer.start();
+
+		add(panel1);
+		
+		setVisible(true);
+		
+		
+	}
+	
+	public void tempfunction() {
+		parentFrame.setVisible(false);
+		parentFrame.dispose();
+	}
+}
---
+++ src/frame/MainFrame.java
@@ -0,0 +1,114 @@
+package frame;
+
+import style.ProjColor;
+import style.ProjStyleButton;
+import style.ProjStyleScrollBar;
+
+import javax.swing.*;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.GridLayout;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.util.ArrayList;
+
+
+public class MainFrame extends JFrame {
+	private ArrayList<Object[]> projectList = new ArrayList<>();
+	private ArrayList<ProjStyleButton> btnArray = new ArrayList<>();
+	private JPanel btnPanel;
+	private JScrollPane scr;
+	private int numBtn = 0;
+	
+	//생성자
+	public MainFrame() {
+		setTitle("Main Frame");
+		setSize(1150,820);
+		setLocationRelativeTo(null); // 화면 중앙 위치
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		
+		// layout manager 전달
+		//setLayout(null);
+		
+		// panel 생성
+		JPanel panel1 = new JPanel(); // 배경 패널
+		panel1.setBackground(ProjColor.customGray);
+		
+		// panel1 레이아웃
+		panel1.setLayout(null);
+		
+		btnPanel = new JPanel(); // 내부 배경 패널
+		btnPanel.setBackground(ProjColor.customDarkGray);
+		btnPanel.setLayout(null);
+		//btnPanel.setBounds(48, 95, 1062, 680);
+		btnPanel.setPreferredSize(new Dimension(1000, 120));;
+		
+		scr = new JScrollPane(btnPanel);
+		scr.setBackground(ProjColor.customDarkGray);
+		scr.setBorder(null);
+		panel1.add(scr);
+		scr.setBounds(48, 95, 1062, 680);
+		scr.setVerticalScrollBar(new ProjStyleScrollBar());
+		scr.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED);
+        scr.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
+		
+		//라벨
+		JLabel lbl1 = new JLabel("All Projects");
+		lbl1.setFont(new Font(null, Font.PLAIN, 50)); // 폰트, 굵게, 크기
+		panel1.add(lbl1);
+		lbl1.setBounds(20,1,400,80);
+		
+		ProjStyleButton btn4 = new ProjStyleButton(ProjColor.customDarkGray, ProjColor.clickedCustomDarkGray, Color.BLACK, "+ new project");
+		panel1.add(btn4);
+		btn4.setBounds(890, 24, 220, 57);
+		btn4.setPreferredSize(new Dimension(220, 57));
+		
+		btn4.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseReleased(MouseEvent e) { // 원래 기능 + new
+            	parent();
+            }
+        });
+		
+		add(panel1);
+		
+		// visible
+		setVisible(true);
+	}
+	
+	public void parent() {
+		new CreateProjectDialog(this);
+	}
+	
+	public void addProjectList(String title, String description) {
+		Object[] array = {title, description};
+		projectList.add(array);
+		
+	}
+	
+	public void repaintButtonPanel() {
+		String title = (String)projectList.get(numBtn)[0];
+		ProjStyleButton tempbtn = new ProjStyleButton(ProjColor.customDarkSkyblue, ProjColor.clickedCustomDarkSkyblue, Color.BLACK, title);
+		btnArray.add(tempbtn);
+		btnArray.get(numBtn).setBounds(31, 35 + 110 * numBtn, 997, 75);
+		btnPanel.add(btnArray.get(numBtn));
+		btnArray.get(numBtn).setPreferredSize(new Dimension(997, 75));
+		btnPanel.setPreferredSize(new Dimension(1000, 120 + 110 * numBtn));
+		
+		btnArray.get(numBtn).addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseReleased(MouseEvent e) {
+				new ProjectFrame(title);
+			}
+		});
+		
+		btnPanel.revalidate();
+		numBtn++;
+		
+		scr.revalidate();
+        scr.repaint();
+	}
+}
+
+
